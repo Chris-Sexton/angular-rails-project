2ca941e1dc2d82b2fe6e417c0cec907d59245704
Chris-Sexton <champlain2013@gmail.com>
2017-01-13 13:49:08 -0500

    Added workouts controller and model.

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,25 @@
+class WorkoutsController < ApplicationController
+
+  def index
+    respond_with Workout.all 
+  end
+
+  def create 
+    respond_with Workout.create(workout_params)
+  end
+
+  def show
+    respond_with Workout.find(params[:id])
+  end
+
+  def destroy
+    respond_with Workout.find(params[:id]).destroy
+  end
+
+  private 
+
+  def workout_params
+    params.require(:workout).permit(:title)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'application#home'
+
+  resources :workouts, only: [:create, :index, :show, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
