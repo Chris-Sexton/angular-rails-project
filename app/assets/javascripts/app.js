54db7cce4214cd8096e868fe17bdf20ec513ba93
Chris-Sexton <champlain2013@gmail.com>
2017-01-18 15:24:29 -0500

    Updated workout form HTML file.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,12 +2,7 @@ angular
   .module('app', ['ui.router', 'templates', 'Devise'])
   .directive('addExercise', function() {
     return {
-      template: '<input placeholder="Exercise" name="exercise" ng-model="exercise" type="text" class="validate" required="required">',
-      link: function($scope, element, attrs) {
-        $scope.clickMe = function() {
-          $scope.workouts.push("hello");
-        }
-      }
+      templateUrl: 'workout/_newWorkout.html'
     }
   })
   .config([
